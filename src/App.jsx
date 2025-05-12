b5712e045fcca275da4d4cf327d35887afa78e79
gitDemo <sy9535393@gmail.com>
2025-05-12 22:40:51 +0530

    create page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Project from './pages/Project'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-3xl text-red-700'>hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/sign-in' element={<Signin />} />
+          <Route path='/sign-up' element={<Signup />} />
+          <Route path='/project' element={<Project />} />
+        </Routes>
+      </BrowserRouter >
     </>
   )
 }
---
+++ src/pages/Project.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Project() {
+    return (
+        <div>
+            <h1>this is Project page</h1>
+
+        </div>
+    )
+}
+
+export default Project
